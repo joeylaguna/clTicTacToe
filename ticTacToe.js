1279579d6ce5338c25c6578866ba8cf4c7c5af56
joeylaguna <elagunaj@gmail.com>
2017-03-13 11:00:37 -0700

    Add addPiece and parseBoard methods

--- ticTacToe.js
+++ ticTacToe.js
@@ -17,13 +17,40 @@ let board = `
 let Game = function() {
   this.board = board;
   this.winner = false;
-  this.piecePlacment = [[0,0,0],[0,0,0],[0,0,0]];
+  this.piecePlacment = {
+  	topLeft: 0,
+  	topMiddle: 0,
+  	topRight: 0,
+  	middleLeft: 0,
+  	middleMiddle: 0,
+  	middleRight: 0,
+  	bottomLeft: 0,
+  	bottomMiddle: 0,
+  	bottomRight: 0
+  }
 };
 
 Game.prototype.showBoard = function() {
   //show current board
   console.log(this.board);
-}
+};
+
+Game.prototype.parseBoard = function() {
+  let optionString = ''
+  for(var position in this.piecePlacment) {
+  	if(this.piecePlacment[position] === 0) {
+  	  optionString+= ' ' + position;
+  	}
+  }
+  console.log('this is optionSTring: ' + optionString);
+};
+
+Game.prototype.addPiece = function() {
+  console.log('Add a piece to the board: ');
+  //method to get current options
+  this.parseBoard();
+};
 
 let newGame = new Game();
-newGame.showBoard();
+newGame.addPiece();
+
