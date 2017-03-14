5dccbe2b9a38e8ec5e0ae9adb898e671bda692f1
joeylaguna <elagunaj@gmail.com>
2017-03-13 19:21:29 -0700

    Refactor

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,9 +1,4 @@
-var readline = require('readline');
-
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+var readlineSync = require('readline-sync');
 
 /*
 needed methods:
@@ -16,103 +11,85 @@ way to switch player
 let Game = function() {
   this.board = [[1,2,3], [4,5,6], [7,8,9]];
   this.player = 'x';
+  this.moves = 0
 };
 
 Game.prototype.showBoard = function() {
   //show current board
-  for(var i = 0; i < this.board.length; i++) {
-  	console.log(this.board[i]);
-  }
+  console.log(`${this.board[0][0]} | ${this.board[0][1]} | ${this.board[0][2]}`)
+  console.log(`${this.board[1][0]} | ${this.board[1][1]} | ${this.board[2][2]}`)
+  console.log(`${this.board[2][0]} | ${this.board[2][1]} | ${this.board[2][2]}`)
 };
 
-Game.prototype.parseBoard = function() {
-  let optionString = ''
-  for(var position in this.piecePlacment) {
-  	if(this.piecePlacment[position] === 0) {
-  	  optionString+=position + '\n';
-  	}
-  }
-  return optionString;
-};
-
-Game.prototype.checkForWinner = function() {
-  let winner = false;
-  let count = {
-  	X: 0,
-  	O: 0
-  };
-  //check horiz
-  for(var i = 0; i < this.board.length; i++ ) {
-  	count['X'] = 0;
-    count['O'] = 0;
-  	for(var j = 0; j < this.board[i].length; j++) {
-  	  if(this.board[i][j] !== 0) {
-  	  	count[this.board[i][j]]++;
-  	  }
-  	}
-  }
-
-  if(count['X'] === 3) {
-  	console.log('horiz X player wins!');
-  } else if(count['Y'] === 3) {
-  	console.log('horiz O player wins!');
-  }
-
-  count['X'] = 0;
-  count['O'] = 0;
-
-  //check vert
-  for(var i = 0; i < this.board.length; i++) {
-  	for(var j = 0; j < this.board.length; j++) {	
-  	  if(this.board[j][i] !== 0) {
-  	  	count[this.board[j][i]]++;
-  	  }
-  	}
-  }
-
-  if(count['X'] === 3) {
-  	console.log('vert X player wins!');
-  } else if(count['Y'] === 3) {
-  	console.log('vert O player wins!');
-  }
-
-  count['X'] = 0;
-  count['Y'] = 0;
-  //check diag starting from index 0
-
-  /*
-   1, 0 ,0
-   0, 1, 0
-   0, 0, 1
-  */
-  
-
-  if(count['X'] === 3) {
-  	console.log('vert X player wins!');
-  } else if(count['Y'] === 3) {
-  	console.log('vert O player wins!');
-  }
-  count['X'] = 0;
-  count['Y'] = 0;
-
-  //check for diag starting from last index
-
+Game.prototype.placePiece = function(row, col) {
+  this.board[row][col] = this.player;
+  this.moves++;
 }
 
-Game.prototype.addPiece = function() {
-	console.log('Add a piece to the board.  Here are your options: ');
-	//method to get current options
-	let options = this.parseBoard();
-	console.log(options);
-	let answer;
-	this.checkForWinner();
-	// rl.question('Select a spot from the list above', function(response) {
-	//   //map response to board
-	//   answer = response;
-	//   rl.close();
-	// });	
+Game.prototype.isInvalidMove = function(move) {
+  console.log('invalid move. Please choose 1-9');
+}
 
-};
+Game.prototype.convertMove = function(move) {
+  let row = Math.floor((move - 1) / 3);
+  let col = (move - 1) % 3;
+  return {row, col};
+}
+
+Game.prototype.promptPlayer = function() {
+  let move;
+  do{
+  	move = readlineSync.question(`Player ${this.player}, place your piece by typing 1-9: `);
+  } while(this.isInvalidMove(move));
+  return this.convertMove(move);
+}
+
+Game.prototype.switchPlayer = function(){
+  this.player === 'x' ? this.player = 'o' : this.player = 'x';
+}
+
+Game.prototype.areAllEqual = function(a, b, c) {
+  return a === b && b === c;
+}
+
+Game.prototype.isWinner = function(row, col) {
+  return this.isRowWinner(row) || this.isColWinner(col) || this.isDiagWinner(row, col);
+}
+
+Game.prototype.isDraw = function() {
+  return this.moves === 9;
+}
+ 
+Game.prototype.isRowWinner = function(row) {
+  return this.areAllEqual(this.board[row][0], this.board[row][1], this.board[row][2]);
+}
+
+Game.prototype.isColWinner = function(col) {
+  return this.areAllEqual(this.board[0][col], this.board[1][col], this.board[2][col]);
+}
+
+Game.prototype.isDiagWinner = function(row, col) {
+  return this.areAllEqual(this.board[0][0], this.board[1][1], this.board[2][2] || this.areAllEqual(this.board[0][2], this.board[1][1], this.board[2][0]));
+}
+
+Game.prototype.printWinner = function(){
+  this.showBoard();
+  console.log(`Player ${this.player} wins!`);
+}
+
+Game.prototype.play = function() {
+  this.showBoard();
+  let {row, col} = this.promptPlayer();
+  this.placePiece(row, col);
+  if(this.isWinner(row, col)) {
+  	this.printWinner();
+  } else if(this.isDraw()) {
+  	this.printDraw();
+  } else {
+  	this.switchPlayer();
+  	this.play();
+  }
+}
 
 let newGame = new Game();
-newGame.addPiece()
+newGame.play();
